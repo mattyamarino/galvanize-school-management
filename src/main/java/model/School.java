e4d74851f44129d445a66d4c9d08490a32012ba1
Matt Yamarino <779649@cognizant.com>
2021-01-26 14:11:04 -0600

    added list fields to school and ability to add or remove from them

--- src/main/java/model/School.java
+++ src/main/java/model/School.java
@@ -1,5 +1,42 @@
 package model;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class School {
 
+    List<Instructor> instructors = new ArrayList<>();
+    List<Student> students = new ArrayList<>();
+    List<Course> courses = new ArrayList<>();
+
+    public Instructor addInstructor(Instructor instructor) {
+        instructors.add(instructor);
+        return instructor;
+    }
+
+    public List<Instructor> terminateInstructor(Instructor instructorToTerminate) {
+        instructors.remove(instructorToTerminate);
+        return instructors;
+    }
+
+    public Student addStudent(Student student) {
+        students.add(student);
+        return student;
+    }
+
+    public List<Student> removeStudent(Student studentToRemove) {
+        students.remove(studentToRemove);
+        return students;
+    }
+
+    public Course addCourse(Course courseToAdd) {
+        courses.add(courseToAdd);
+        return courseToAdd;
+    }
+
+    public List<Course> removeCourse(Course courseToRemove) {
+        courses.remove(courseToRemove);
+        return courses;
+    }
 }
--- src/test/java/model/SchoolTest.java
+++ src/test/java/model/SchoolTest.java
@@ -1,6 +1,77 @@
 package model;
 
 
-public class SchoolTest{
+import org.junit.Before;
+import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SchoolTest{
+    School school;
+
+    @Before
+    public void setup() {
+        school = new School();
+    }
+
+    @Test
+    public void addInstructor_addsInstructorToSchool() {
+        Instructor instructor = new Instructor();
+        Instructor result = school.addInstructor(instructor);
+        assertEquals(instructor, result);
+        assertEquals(instructor, school.instructors.get(0));
+    }
+
+    @Test
+    public void terminateInstructor_removesInstructorFromList_andReturnsList(){
+        Instructor instructor1 = new Instructor();
+        Instructor instructor2 = new Instructor();
+        school.instructors.add(instructor1);
+        school.instructors.add(instructor2);
+        List<Instructor> expected = Collections.singletonList(instructor2);
+        List<Instructor> result = school.terminateInstructor(instructor1);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void addStudent_addsStudentToSchool() {
+        Student student = new Student();
+        Student result = school.addStudent(student);
+        assertEquals(student, result);
+        assertEquals(student, school.students.get(0));
+    }
+
+    @Test
+    public void removeStudent_removesStudentFromList_andReturnsList() {
+        Student student1 = new Student();
+        Student student2 = new Student();
+        school.students.add(student1);
+        school.students.add(student2);
+        List<Student> expected = Collections.singletonList(student1);
+        List<Student> result = school.removeStudent(student2);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void addCourse_addsCourseToSchool() {
+        Course course = new Course();
+        Course result = school.addCourse(course);
+        assertEquals(course, result);
+        assertEquals(course, school.courses.get(0));
+    }
+
+    @Test
+    public void removeCourse_removesCourseFromList_andReturnsList() {
+        Course course1 = new Course();
+        Course course2 = new Course();
+        school.courses.add(course1);
+        school.courses.add(course2);
+        List<Course> expected = Collections.singletonList(course1);
+        List<Course> result = school.removeCourse(course2);
+        assertEquals(expected, result);
+    }
 }
